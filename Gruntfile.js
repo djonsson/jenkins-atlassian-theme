bc0870f0cc541bfdcc39892586ce6522bd9c4760
Daniel Jonsson <daniel.jonsson@viaplay.com>
2014-09-03 12:58:41 +0200

    Removed Gravatar and animated progressCircles as they are not functioning properly

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         },
         js : {
             src : [
-                'src/*.js'
+                'src/login.js'
             ],
             dest : 'dist/theme.js'
         }
